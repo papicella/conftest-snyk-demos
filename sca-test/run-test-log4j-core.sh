1d6641853deb6b7b6fad54bf9ea819ef8b5f809b
JennySnyk <81160535+JennySnyk@users.noreply.github.com>
2025-06-08 20:49:36 +0100

    Update run-test-log4j-core.sh

--- sca-test/run-test-log4j-core.sh
+++ sca-test/run-test-log4j-core.sh
@@ -1,1 +1,1 @@
-snyk test --json ./snyk-boot-web | conftest test --policy=./policy/log4j-core-test.rego -
+snyk test --json ./snyk-boot-web | conftest test --rego-version v0 --policy=./policy/log4j-core-test.rego -
